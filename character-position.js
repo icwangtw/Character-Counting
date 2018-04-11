c1809460d9b43cafb4817c63f70a2386773f8695
Stephen <stephenicwang@gmail.com>
2018-04-11 18:40:24 +0000

    first attempt

---
+++ character-position.js
@@ -0,0 +1,14 @@
+function charPosition(input) {
+  var noSpaces = input.split(" ").join("").toLowerCase();
+  var letters = {};
+  for (var i = 0; i < noSpaces.length; i++) {
+    if (letters[noSpaces[i]] === undefined) {
+      letters[noSpaces[i]] = i.toString();
+    }
+    else {
+      letters[noSpaces[i]] = letters[noSpaces[i]] + " & " + i;
+    }
+  }
+  return letters;
+}
+console.log(charPosition("hello how are you"))
